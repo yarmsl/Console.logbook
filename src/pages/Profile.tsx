4578a2d264dad1fbc72818c38926dd6b0ba61cab
Yaroslav Makarov <slideryo@gmail.com>
2021-11-01 22:42:50 +0500

    in progress

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,56 @@
-import {  TextField } from "@mui/material";
-import { ReactElement } from "react";
+import { Button } from "@mui/material";
+import { useRef, ReactElement, useState } from "react";
 import HelmetTitle from "../layouts/Helmet";
 
 const Profile = (): ReactElement => {
+  const [photo, setPhoto] = useState<Blob>();
+  const [blobPhoto, setBlobPhoto] = useState<string | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleUpload = (e: any) => {
+    let box = null;
+    if (inputRef.current && inputRef.current.files?.length === 1) {
+      box = inputRef.current?.files[0];
+    }
+
+    if (box != null) {
+      const reader = new FileReader();
+      //loading start
+      reader.readAsArrayBuffer(box);
+      reader.onload = () => {
+        if (reader.result != null && reader.result instanceof ArrayBuffer) {
+          const res = reader.result;
+          const ablob = new Blob([res]);
+          setPhoto(ablob);
+        }
+      };
+    }
+  };
+
+  const sendFile = () => {
+    const sendData = new FormData();
+    if (photo) {
+      sendData.append("photo", photo);
+      fetch("/1234567", {
+        method: "POST",
+        // headers: {
+        //   "Content-Type": "multipart/form-data",
+        // },
+        body: sendData,
+      });
+    }
+  };
+
   return (
     <>
       <HelmetTitle title="Profile" />
-      <TextField type='file' />
+      <input
+        ref={inputRef}
+        onChange={handleUpload}
+        type="file"
+        accept="image/png, image/jpeg, image/webp"
+      />
+      <Button onClick={sendFile}>SEND</Button>
     </>
   );
 };
